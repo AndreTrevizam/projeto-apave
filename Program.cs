fcc0bc378a25f48c00e20d3b3a337720dd62ff56
AndreTrevizam <andretrevizam264@gmail.com>
2025-04-21 22:37:23 -0300

    Adiciona rota default e autentificação

--- Program.cs
+++ Program.cs
@@ -3,26 +3,38 @@ using projeto_apave.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
+// Serviços
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<DbApave>(options => 
+builder.Services.AddDbContext<DbApave>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddSession();
+
+builder.Services.AddAuthentication("Autentificacao")
+    .AddCookie("Autentificacao", options =>
+    {
+        options.LoginPath = "/Usuario/Login";
+        options.LogoutPath = "/Usuario/Logout";
+    });
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
+// Middlewares
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapStaticAssets();
+app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+    pattern: "{controller=Usuario}/{action=Login}/{id?}"
+);
 
 app.Run();
