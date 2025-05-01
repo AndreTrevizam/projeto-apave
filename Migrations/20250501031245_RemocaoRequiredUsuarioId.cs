074e2ed85080286ca551e202baf81b80611d970f
AndreTrevizam <andretrevizam264@gmail.com>
2025-05-01 15:00:56 -0300

    Adiciona migrações para ajustes no modelo PainelPeca e remoção da obrigatoriedade do campo UsuarioId

---
+++ Migrations/20250501031245_RemocaoRequiredUsuarioId.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace projeto_apave.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemocaoRequiredUsuarioId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Painel_Usuario_UsuarioId",
+                table: "Painel");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Painel_Usuario_UsuarioId",
+                table: "Painel",
+                column: "UsuarioId",
+                principalTable: "Usuario",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Painel_Usuario_UsuarioId",
+                table: "Painel");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Painel_Usuario_UsuarioId",
+                table: "Painel",
+                column: "UsuarioId",
+                principalTable: "Usuario",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
