1ddb6195c424a5744bcf996e4da258a26c3796f9
AndreTrevizam <andretrevizam264@gmail.com>
2025-05-01 15:03:08 -0300

    Refatorado Painel pra mais consistência nas declarações

--- Models/Painel.cs
+++ Models/Painel.cs
@@ -1,31 +1,37 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-
+namespace projeto_apave.Models;
 public class Painel
 {
-  [Key]
-  public int Id { get; set; }
+    [Key]
+    public int Id { get; set; }
 
-  public string Nome { get; set; }
+    [Required]
+    public string Nome { get; set; }
 
-  public string Descricao { get; set; }
+    [Required]
+    public string Descricao { get; set; }
 
-  [Required]
-  public double Altura { get; set; }
-  public double Largura { get; set; }
-  public double Comprimento { get; set; }
+    [Required]
+    [Range(0.01, double.MaxValue)]
+    public double Altura { get; set; }
 
-  public DateTime DataCriacao { get; set; } = DateTime.Now;
-  public DateTime? DataAlteracao { get; set; }
-  public List<Manutencao> Manutencoes { get; set; }
+    [Range(0.01, double.MaxValue)]
+    public double Largura { get; set; }
 
-  public ICollection<PainelPeca> Pecas { get; set; } = new List<PainelPeca>();
+    [Range(0.01, double.MaxValue)]
+    public double Comprimento { get; set; }
 
-   // RELACIONAMENTO COM USUÁRIO
-  [Required]
-  public int UsuarioId { get; set; }   // FK (chave estrangeira)
+    public DateTime DataCriacao { get; set; } = DateTime.Now;
+    public DateTime? DataAlteracao { get; set; }
 
-  [ForeignKey("UsuarioId")]
-  public Usuario Usuario { get; set; } // Navegação
+    public List<Manutencao> Manutencoes { get; set; } = new List<Manutencao>();
+
+    public ICollection<PainelPeca> Pecas { get; set; } = new List<PainelPeca>();
+
+    public int UsuarioId { get; set; }
+
+    [ForeignKey("UsuarioId")]
+    public Usuario Usuario { get; set; }
 }
