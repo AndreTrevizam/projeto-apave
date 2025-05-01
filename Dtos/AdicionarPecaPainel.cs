3cab4d3282a1a3613e9678a05d8e760fccbce682
AndreTrevizam <andretrevizam264@gmail.com>
2025-05-01 15:00:44 -0300

    Removido DTO AdicionarPecaPainelDto já que não é mais preciso

--- Dtos/AdicionarPecaPainel.cs
+++
@@ -1,7 +0,0 @@
-public class AdicionarPecaPainelDto
-{
-    public int PainelId { get; set; }
-    public int PecaId { get; set; }
-    public int Quantidade { get; set; }
-    public DateTime DataInstalacao { get; set; } = DateTime.Now;
-}
