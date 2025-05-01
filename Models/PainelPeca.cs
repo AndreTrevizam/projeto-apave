7e7910da408b17051e7d193c58da004738cddc66
AndreTrevizam <andretrevizam264@gmail.com>
2025-05-01 15:03:23 -0300

    Refatoração do código em PainelPeca para melhorar a consistência na formatação

--- Models/PainelPeca.cs
+++ Models/PainelPeca.cs
@@ -1,21 +1,23 @@
+namespace projeto_apave.Models;
 public class PainelPeca
 {
-  public int PainelId { get; set; }
-  public Painel Painel { get; set; }
+    public int PainelId { get; set; }
+    public Painel Painel { get; set; }
 
-  public int PecaId { get; set; }
-  public Peca Peca { get; set; }
+    public int PecaId { get; set; }
+    public Peca Peca { get; set; }
 
-  public int Quantidade { get; set; }
+    public int Quantidade { get; set; }
 
-  public DateTime DataInstalacao { get; set; }
-  public DateTime DataCriacao { get; set; } = DateTime.Now;
-  public DateTime? DataAtualizacao { get; set; }
+    public DateTime DataInstalacao { get; set; }
+    public DateTime DataCriacao { get; set; } = DateTime.Now;
+    public DateTime? DataAtualizacao { get; set; }
 
-  public StatusPainelPeca Status { get; set; } = StatusPainelPeca.Ativo;
+    public StatusPainelPeca Status { get; set; } = StatusPainelPeca.Ativo;
+}
 
-  public enum StatusPainelPeca {
-      Ativo,
-      Inativo
-  }
+public enum StatusPainelPeca
+{
+    Ativo,
+    Inativo
 }
