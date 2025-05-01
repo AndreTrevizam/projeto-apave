10258913cf548ae18a2473f03924fa3a880286e8
AndreTrevizam <andretrevizam264@gmail.com>
2025-05-01 15:00:04 -0300

    Removido métodos desnecessários

--- Controllers/FuncionarioController.cs
+++ Controllers/FuncionarioController.cs
@@ -3,7 +3,6 @@ using projeto_apave.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.Threading.Tasks;
 
 namespace projeto_apave.Controllers
 {
@@ -25,18 +24,6 @@ namespace projeto_apave.Controllers
             return View(paineis);
         }
 
-        [Authorize(Roles = "Funcionario")]
-        public async Task<IActionResult> AdicionarPeca()
-        {
-            var paineis = await _db.Painel.ToListAsync();
-            var pecas = await _db.Peca.ToListAsync();
-
-            ViewBag.Paineis = paineis;
-            ViewBag.Pecas = pecas;
-
-            return View();
-        }
-
         public async Task<IActionResult> AprovarSolicitacoes()
         {
             var solicitacoesPendentes = await _db.SolicitacaoPainel
@@ -54,10 +41,8 @@ namespace projeto_apave.Controllers
             if (solicitacao == null)
                 return NotFound();
 
-            // Atualiza o status para Aprovado
             solicitacao.Status = StatusSolicitacao.Aprovado;
 
-            // Cria o novo painel usando os dados da solicitação
             var novoPainel = new Painel
             {
                 Nome = solicitacao.NomePainel,
@@ -67,10 +52,9 @@ namespace projeto_apave.Controllers
             };
 
             _db.Painel.Add(novoPainel);
-
             await _db.SaveChangesAsync();
 
-            return RedirectToAction("AprovarSolicitacoes");
+            return RedirectToAction("GerenciarPaineis");
         }
 
         [HttpPost]
@@ -80,41 +64,12 @@ namespace projeto_apave.Controllers
             if (solicitacao == null)
                 return NotFound();
 
-            // Atualiza o status para Recusado
             solicitacao.Status = StatusSolicitacao.Recusado;
-
-            // Deletar a solicitação recusada:
-            _db.SolicitacaoPainel.Remove(solicitacao);
-
             await _db.SaveChangesAsync();
 
             return RedirectToAction("AprovarSolicitacoes");
         }
 
-        [HttpPost]
-        public async Task<IActionResult> AdicionarPeca([FromBody] AdicionarPecaPainelDto dto)
-        {
-            var painel = await _db.Painel.FindAsync(dto.PainelId);
-            var peca = await _db.Peca.FindAsync(dto.PecaId);
 
-            if (painel == null || peca == null)
-            {
-                return NotFound("Painel ou Peça não encontrados.");
-            }
-
-            var painelPeca = new PainelPeca
-            {
-                PainelId = dto.PainelId,
-                PecaId = dto.PecaId,
-                Quantidade = dto.Quantidade,
-                DataInstalacao = dto.DataInstalacao,
-                Status = PainelPeca.StatusPainelPeca.Ativo
-            };
-
-            _db.PainelPeca.Add(painelPeca);
-            await _db.SaveChangesAsync();
-
-            return Ok(new { mensagem = "Peça adicionada ao painel com sucesso!" });
-        }
     }
 }
